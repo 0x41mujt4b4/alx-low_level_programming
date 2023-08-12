124f07f41797f61c89aa536a365faaf7b984d485
0x41mujt4b4 <almujtaba007@gmail.com>
2023-08-11 19:58:47 -0700

    update 101

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -5,25 +5,25 @@
  */
 int main(void)
 {
-    int i, j;
+	int i, j;
 
     for (i = 0; i < 100; i++)
     {
-        for (j = i + 1; j < 100; j++)
-        {
-            putchar(i / 10 + '0');
-            putchar(i % 10 + '0');
-            putchar(' ');
-            putchar(j / 10 + '0');
-            putchar(j % 10 + '0');
+	for (j = i + 1; j < 100; j++)
+	{
+	putchar(i / 10 + '0');
+	    putchar(i % 10 + '0');
+	    putchar(' ');
+	    putchar(j / 10 + '0');
+	    putchar(j % 10 + '0');
 
-            if (i != 98 || j != 99)
-            {
-                putchar(',');
-                putchar(' ');
-            }
-        }
-    }
+	    if (i != 98 || j != 99)
+	{
+		putchar(',');
+		putchar(' ');
+}
+	}
+}
     putchar('\n');
     return (0);
 }
