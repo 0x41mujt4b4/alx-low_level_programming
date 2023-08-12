87729272baeff03a06a98ecce323eb62f2c9bead
0x41mujt4b4 <almujtaba007@gmail.com>
2023-08-11 19:02:47 -0700

    update 3-

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -9,15 +9,16 @@ int main(void)
 {
 	int x;
 
-	for(x = 'A'; x <= 'Z'; x++)
+	for (x = 'A'; x <= 'Z'; x++)
 	{
 		int lower_x = tolower(x);
+
 		putchar(lower_x);
 	}
-	for(x = 'A'; x <= 'Z'; x++)
+	for (x = 'A'; x <= 'Z'; x++)
         {
-                putchar(x);
-        }
+		putchar(x);
+	}
 	putchar('\n');
 	return (0);
 }
