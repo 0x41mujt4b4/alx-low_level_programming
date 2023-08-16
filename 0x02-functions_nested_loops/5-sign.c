1693d8f06373a1fa27cd6cc91b192c8ca63c33be
0x41mujt4b4 <almujtaba007@gmail.com>
2023-08-16 13:00:23 -0700

    add function print_sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+/**
+ * print_sign - check number sign
+ * @n: integer
+ * Return: 0 or 1 or -1
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n < 0)
+	{
+		_putchar('-');
+		return (-1);
+	}
+	_putchar(0);
+	return (0);
+}
