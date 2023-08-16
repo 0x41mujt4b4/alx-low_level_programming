a7d58577e5cba49622021e65343091bc131f9389
0x41mujt4b4 <almujtaba007@gmail.com>
2023-08-16 13:44:38 -0700

    add print_last_digit function

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/* betty style doc for function main goes there */
+/**
+ * print_last_digit - get lasr digit
+ * @n: integer
+ ** Return: integer
+ */
+int print_last_digit(int n)
+{
+	return (n % 10);
+}
