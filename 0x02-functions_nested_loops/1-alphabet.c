ec1effbaf4816cebe35bc96e2647a74a19a3981e
0x41mujt4b4 <almujtaba007@gmail.com>
2023-08-16 09:55:46 -0700

    add file

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char *c = "abcdefghijklmnopqrstuvwxyz\n";
+
+	while (*c)
+		_putchar(*c++);
+	return (0);
+}
