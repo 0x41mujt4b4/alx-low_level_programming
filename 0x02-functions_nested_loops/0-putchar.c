11e9acb9b568d32dc20a3d9ea578cf55c865fd8b
0x41mujt4b4 <almujtaba007@gmail.com>
2023-08-16 09:31:18 -0700

    update file

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -8,8 +8,11 @@
  * Return: Always 0 (Success)
  */
 
-int main(){
-	char* c = "_putchar\n";
-	while (*c) _putchar(*c++);
-	return 0;
+int main(void)
+{
+	char *c = "_putchar\n";
+
+	while (*c)
+		_putchar(*c++);
+	return (0);
 }
