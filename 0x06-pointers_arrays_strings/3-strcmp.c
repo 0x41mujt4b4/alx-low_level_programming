67bca2bec179af03f63933f5a7c4662a200aff3d
0x41mujt4b4 <almujtaba007@gmail.com>
2023-08-24 08:04:29 -0700

    update 3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -8,13 +8,13 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-			int i = 0;
+		int c1, c2;
+		int i = 0;
 
 		while ((s1[i] == s2[i]) && (s1[i] != '\0'))
 			i++;
-		int c1 = s1[i];
-
-		int c2 = s2[i];
+		c1 = s1[i];
+		c2 = s2[i];
 
 		return (s1[i] == s2[i] ? 0 : c1 > c2 ? 1 : -1);
 }
