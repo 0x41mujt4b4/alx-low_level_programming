1a6d45261037428750f1c02d8e52b554f62c9ec0
0x41mujt4b4 <almujtaba007@gmail.com>
2023-08-24 08:19:11 -0700

    update 3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -4,17 +4,15 @@
  * _strcmp - compre two strings
  * @s1: destination string
  * @s2: source string
- * Return: 0, 1, or -1
+ * Return: positive negative or zero
  */
 int _strcmp(char *s1, char *s2)
 {
-		int c1, c2;
-		int i = 0;
+	while (*s1 && (*s1 == *s2))
+	{
+		s1++;
+		s2++;
+	}
 
-		while ((s1[i] == s2[i]) && (s1[i] != '\0'))
-			i++;
-		c1 = s1[i];
-		c2 = s2[i];
-
-		return (s1[i] == s2[i] ? 0 : c1 > c2 ? 1 : -1);
+	return (*(const unsigned char)*s1 - *(const unsigned char)*s2);
 }
