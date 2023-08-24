b4eb23418844f05b5956db24074b2e57bd7129ce
0x41mujt4b4 <almujtaba007@gmail.com>
2023-08-24 07:53:11 -0700

    add 3-strcmp.c

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * _strcmp - compre two strings
+ * @s1: destination string
+ * @s2: source string
+ * Return: 0, 1, or -1
+ */
+int _strcmp(char *s1, char *s2)
+{
+			int i = 0;
+
+		while ((s1[i] == s2[i]) && (s1[i] != '\0'))
+			i++;
+		int c1 = s1[i];
+
+		int c2 = s2[i];
+
+		return (s1[i] == s2[i] ? 0 : c1 > c2 ? 1 : -1);
+}
