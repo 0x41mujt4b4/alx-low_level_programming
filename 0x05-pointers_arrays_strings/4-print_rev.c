1591556eb5981c5527db8d30c95771ba720ac65f
0x41mujt4b4 <almujtaba007@gmail.com>
2023-08-26 00:01:27 -0700

    4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_rev - print string reversed
+ * @s: string
+ *
+ * Return: Nothing
+ */
+void print_rev(char *s)
+{
+	int i = 0;
+
+	while (*(s + i))
+		i++;
+	while (i + 1)
+	{
+		printf("%c", *(s + i));
+		i--;
+	}
+	printf("\n");
+}
