ae7966ba45af621633e401f3ddcd28067d318cbd
0x41mujt4b4 <almujtaba007@gmail.com>
2023-08-26 02:37:26 -0700

    add 6-puts2

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -2,18 +2,18 @@
 #include "main.h"
 
 /**
- * puts_half - print every other character started from 1th
+ * puts2 - print every other character started from 1th
  * @str: pointer var of type str
  *
  * Return: length of string
  */
-void puts_half(char *str)
+void puts2(char *str)
 {
 	int i = 0;
 
 	while (*(str + i))
 	{
-		if (i%2 == 0)
+		if (i % 2 == 0)
 		printf("%c", *(str + i));
 		i++;
 	}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -3,4 +3,5 @@ void swap_int(int *a, int *b);
 int _strlen(char *s);
 void _puts(char *str);
 void rev_string(char *s);
+void puts2(char *str);
 void puts_half(char *str);
