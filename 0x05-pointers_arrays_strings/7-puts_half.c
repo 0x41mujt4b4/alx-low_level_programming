8f3ddac630f5c45a12babf046eda00a11511023b
0x41mujt4b4 <almujtaba007@gmail.com>
2023-08-26 02:47:15 -0700

    add 7-puts_half.c

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * puts_half - print half string
+ * @str: pointer var of type str
+ *
+ * Return: Nothing
+ */
+void puts_half(char *str)
+{
+	int i = 0;
+
+	while (*(str + i))
+	{
+		i++;
+	}
+	if (i % 2)
+		i = (i - 1) / 2;
+	i /= 2;
+	while (*(str + i))
+	{
+		printf("%c", *(str + i));
+		i++;
+	}
+	printf("\n");
+}
