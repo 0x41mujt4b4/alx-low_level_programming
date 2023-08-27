4b50948544f8c6a36e7b73d66fa4b632fed2b359
0x41mujt4b4 <almujtaba007@gmail.com>
2023-08-27 06:12:36 -0700

    update 100-atoi.c

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -2,52 +2,41 @@
 #include "main.h"
 
 /**
- * isInt - check whether char is int
- * @c: char
+ * _atoi - Convert a string to an integer.
+ * @s: The pointer to convert
  *
- * Return: int
- */
-int isInt(int c)
-{
-	return ((c - 48 >= 0 && c - 48 <= 9) ? 1 : 0);
-}
-
-/**
- * isSign - check whether given value is - or +
- * @c: char
- *
- * Return: int
- */
-int isSign(int c)
-{
-	return (c == 45 ? -1 : c == 43 ? 1 : 0);
-}
-
-/**
- * _atoi - convert char to int
- * @s: string
- *
- * Return: int
+ * Return: A integer
  */
 int _atoi(char *s)
 {
-		int i = 0;
-		int sign = 1;
-		int n = 0;
+	int c = 0;
+	unsigned int ni = 0;
+	int min = 1;
+	int isi = 0;
 
-		while (!isInt(s[i]))
+	while (s[c])
+	{
+		if (s[c] == 45)
 		{
-			if (s[i] == '\0')
-				return (0);
-			if (isSign(s[i]))
-				sign = sign * isSign(s[i]);
-			i++;
+			min *= -1;
 		}
 
-		while (isInt(s[i]))
+		while (s[c] >= 48 && s[c] <= 57)
 		{
-			n = (10 * n) + (s[i] - 48);
-			i++;
+			isi = 1;
+			ni = (ni * 10) + (s[c] - '0');
+			c++;
 		}
-		return (n * sign);
+
+		if (isi == 1)
+		{
+			break;
+		}
+
+		c++;
+	}
+
+	ni *= min;
+	return (ni);
 }
+
