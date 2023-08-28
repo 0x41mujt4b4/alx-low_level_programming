51ab3abd9745ffa972777084c55503f88ef750a4
0x41mujt4b4 <almujtaba007@gmail.com>
2023-08-28 07:02:17 -0700

    update 3-strspn.c

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,23 +1,33 @@
-#include <stdio.h>
-#include "main.h"
+#include "holberton.h"
 
 /**
- * _strspn - convert
- *
- * @s: string to be scanned
- * @accept: string containing the characters to match
- *
- * Return: length of matching segment
- */
+  * _strspn - search a string for a set of bytes
+  * @s: source string
+  * @accept: accepted string
+  *
+  * Return: number of bytes in the init segment
+  */
 unsigned int _strspn(char *s, char *accept)
 {
-	int i = 0;
+	unsigned int a = 0, b, t = 0;
 
-	while (s[i])
+	while (accept[a])
 	{
-		if (!_strchr(accept, s[i]))
-			break;
-		i++;
+		b = 0;
+
+		while (s[b] != 32)
+		{
+			if (accept[a] == s[b])
+			{
+				t++;
+			}
+
+			b++;
+		}
+
+		a++;
 	}
-	return (i);
+
+	return (t);
 }
+
