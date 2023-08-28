0b934f459eccf1c64af173a179ee28ec1728aac2
0x41mujt4b4 <almujtaba007@gmail.com>
2023-08-28 02:23:37 -0700

    add 2-strchr.c

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _strchr - convert
+ *
+ * @s: string to search
+ * @c: character to locate
+ *
+ * Return: pointer
+ */
+char *_strchr(char *s, char c)
+{
+	int i = 0;
+	char *ptr = NULL;
+
+	while (s[i++])
+		if (s[i] == c)
+		{
+			ptr = &s[i];
+			break;
+		}
+	return (ptr);
+}
