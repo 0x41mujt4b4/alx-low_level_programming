54e5fc24c4cff32ac953f49102fd4b0bc045d0e3
0x41mujt4b4 <almujtaba007@gmail.com>
2023-09-03 22:42:15 -0700

    update 0-create_array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -18,7 +18,7 @@ char *create_array(unsigned int size, char c)
 	char *s;
 
 	s = (char *) malloc(size * sizeof(char));
-	if (s == NULL)
+	if (s == NULL || size == 0)
 		return (NULL);
 	for (a = 0; a < size; a++)
 	{
