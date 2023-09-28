47e31f88c3d604df6c1451db86da73e972c35831
0x41mujt4b4 <almujtaba007@gmail.com>
2023-09-28 00:58:41 -0700

    add 0-binary_to_uint.c

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * binary_to_uint - convert binary to decimal
+ * @b: the binary to convert
+ *
+ * Return: converted binary
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int deci = 0;
+	int counter = 0, mult = 1;
+
+	if (b == NULL)
+		return (0);
+	while (b[counter])
+	{
+		if (b[counter] != '0' && b[counter] != '1')
+			return (0);
+		counter++;
+	}
+	counter--;
+	while (b[counter])
+	{
+		if (b[counter] == '1')
+			deci += mult;
+		mult *= 2;
+		counter--;
+	}
+	return (deci);
+
+}
