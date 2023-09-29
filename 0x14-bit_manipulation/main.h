558117d401575bc6bf04227c344713a912feb370
0x41mujt4b4 <almujtaba007@gmail.com>
2023-09-29 10:42:39 -0700

    update main.h

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,5 +1,6 @@
 #ifndef HEADER_FILE
 #define HEADER_FILE
+#include <stdio.h>
 unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
 void print_binary(unsigned long int n);
