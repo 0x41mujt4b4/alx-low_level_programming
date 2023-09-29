773162c26c10f8c9e4c1f17cc288790c71710158
0x41mujt4b4 <almujtaba007@gmail.com>
2023-09-29 10:25:54 -0700

    add 1-print_binary.c

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,31 +1,13 @@
-#include <stdio.h>
-#include "main.h"
+#include "holberton.h"
 
 /**
- * print_binary - print the binary represntation of a number
- * @n: the number to be converted to binary
- *
- * Return: Nothing
+ * print_binary - prints the binary representation of a number
+ * @n: number
  */
 void print_binary(unsigned long int n)
 {
-	int bit;
+	if (n > 1)
+		print_binary(n >> 1);
 
-	if (n == 0)
-	{
-		printf("0");
-		return;
-	}
-
-	bit = sizeof(n) * 8 - 1; /* Find the highest bit */
-	while ((n & (1 << bit)) == 0) /* Shift until we find the first '1' */
-	{
-		bit--;
-	}
-
-	/* Print the bits from most significant to least significant */
-	for (; bit >= 0; bit--)
-	{
-		printf("%d", (n & (1 << bit)) ? 1 : 0);
-	}
+	_putchar((n & 1) + '0');
 }
