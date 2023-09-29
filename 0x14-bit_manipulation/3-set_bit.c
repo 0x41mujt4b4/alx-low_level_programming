d980bca6fee041a15e03ae4aab8ccfbf0aa8a613
0x41mujt4b4 <almujtaba007@gmail.com>
2023-09-29 12:14:05 -0700

    add 3-set_bit.c

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -11,7 +11,8 @@
  */
 int set_bit(unsigned long int *n, unsigned int index)
 {
-	if (index < 64)
-		return (*n |= (1 << index));
-	return (-1);
+	if (index > 63)
+		return (-1);
+	*n |= (1 << index);
+	return (1);
 }
