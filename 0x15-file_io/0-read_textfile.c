0ab9689d76974538db727d75a0ced64662255854
0x41mujt4b4 <almujtaba007@gmail.com>
2023-10-02 02:39:40 -0700

    add 0-read_textfile.c

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include "main.h"
+
+/**
+ * read_textfile - read text from file
+ *
+ * @filename: the file name
+ * @letters: the number of letters to read
+ *
+ * Return: the number of readed bytes
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd;
+	char *buffer;
+	ssize_t bytes_read, bytes_written;
+	
+	if (filename == NULL)
+		return (0);
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+		return (0);
+	buffer = malloc(sizeof(char) * letters);
+	if (buffer == NULL)
+		return (0);
+	bytes_read = read(fd, buffer, letters);
+	if (bytes_read == -1)
+		return (0);
+	bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
+	if (bytes_written == -1)
+		return (0);
+	free(buffer);
+	close(fd);
+	return (bytes_read);
+
+}
