8554d6a702b0fe406c0ad5ed6105cd165e217d3e
0x41mujt4b4 <almujtaba007@gmail.com>
2023-11-09 12:03:29 -0800

    update 8-

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -1,202 +1,36 @@
 #include "lists.h"
-
 /**
- * dlistint_len - Returns number of elements in linked list
- * @h: Pointer to first node of linked list
- *
- * Return: Number of elements in linked list
- */
-size_t dlistint_len(const dlistint_t *h)
-{
-	const dlistint_t *current;
-	size_t n;
-
-	current = h;
-	n = 0;
-	while (current)
-	{
-		current = current->next;
-		++n;
-	}
-	return (n);
-}
-
-/**
- * delete_dnodeint_at_index - Deletes the node at index
- * @head: Pointer to a pointer of first node
- * @index: Index of node that should be deleted starting at 0
- *
- * Return: 1 on success or -1 on failure
+ * delete_dnodeint_at_index - delet  a node in a given position
+ * @head: double pointer to structure
+ * @index: index of node to delete from the list
+ * Return: 1 sucess, -1 failure
  */
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
-	dlistint_t *current, *tmp;
-	unsigned int i;
+	dlistint_t *tmp;
+	unsigned int nth = 0;
 
-	if (!*head || index > dlistint_len(*head))
-		return (-1);
 	tmp = *head;
-	current = *head;
-	i = 1;
-
-	while (i < index)
+	if (*head == NULL)
+		return (-1);
+	if (index == 0)
 	{
-		if (!tmp)
-			return (-1);
-		++i;
-		tmp = tmp->next;
-	}
-	if (index)
-	{
-		current = tmp->next;
-		tmp->next = current->next;
 		if (tmp->next)
-		{
-			tmp = tmp->next;
-			tmp->prev = current->prev;
-		}
+			tmp->next->prev = NULL;
+		(*head) = (*head)->next;
+		free(tmp);
+		return (1);
 	}
-	else
+	while (tmp && nth < index)
 	{
-		*head = (*head)->next;
-		if (*head)
-			(*head)->prev = NULL;
-
+		tmp = tmp->next;
+		nth++;
 	}
-	free(current);
+	if (tmp == NULL)
+		return (-1);
+	tmp->prev->next = tmp->next;
+	if (tmp->next)
+		tmp->next->prev = tmp->prev;
+	free(tmp);
 	return (1);
-#include "lists.h"
-
-/**
- * dlistint_len - Returns number of elements in linked list
- * @h: Pointer to first node of linked list
- *
- * Return: Number of elements in linked list
- */
-size_t dlistint_len(const dlistint_t *h)
-{
-	const dlistint_t *current;
-	size_t n;
-
-	current = h;
-	n = 0;
-	while (current)
-	{
-		current = current->next;
-		++n;
-	}
-	return (n);
 }
-
-/**
- * delete_dnodeint_at_index - Deletes the node at index
- * @head: Pointer to a pointer of first node
- * @index: Index of node that should be deleted starting at 0
- *
- * Return: 1 on success or -1 on failure
- */
-int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
-{
-	dlistint_t *current, *tmp;
-	unsigned int i;
-
-	if (!*head || index > dlistint_len(*head))
-		return (-1);
-	tmp = *head;
-	current = *head;
-	i = 1;
-
-	while (i < index)
-	{
-		if (!tmp)
-			return (-1);
-		++i;
-		tmp = tmp->next;
-	}
-	if (index)
-	{
-		current = tmp->next;
-		tmp->next = current->next;
-		if (tmp->next)
-		{
-			tmp = tmp->next;
-			tmp->prev = current->prev;
-		}
-	}
-	else
-	{
-		*head = (*head)->next;
-		if (*head)
-			(*head)->prev = NULL;
-
-	}
-	free(current);
-	return (1);
-#include "lists.h"
-
-/**
- * dlistint_len - Returns number of elements in linked list
- * @h: Pointer to first node of linked list
- *
- * Return: Number of elements in linked list
- */
-size_t dlistint_len(const dlistint_t *h)
-{
-	const dlistint_t *current;
-	size_t n;
-
-	current = h;
-	n = 0;
-	while (current)
-	{
-		current = current->next;
-		++n;
-	}
-	return (n);
-}
-
-/**
- * delete_dnodeint_at_index - Deletes the node at index
- * @head: Pointer to a pointer of first node
- * @index: Index of node that should be deleted starting at 0
- *
- * Return: 1 on success or -1 on failure
- */
-int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
-{
-	dlistint_t *current, *tmp;
-	unsigned int i;
-
-	if (!*head || index > dlistint_len(*head))
-		return (-1);
-	tmp = *head;
-	current = *head;
-	i = 1;
-
-	while (i < index)
-	{
-		if (!tmp)
-			return (-1);
-		++i;
-		tmp = tmp->next;
-	}
-	if (index)
-	{
-		current = tmp->next;
-		tmp->next = current->next;
-		if (tmp->next)
-		{
-			tmp = tmp->next;
-			tmp->prev = current->prev;
-		}
-	}
-	else
-	{
-		*head = (*head)->next;
-		if (*head)
-			(*head)->prev = NULL;
-
-	}
-	free(current);
-	return (1);
-}}}
