40de2cc48e17070d1e1729d5de61c807ba5a07fd
0x41mujt4b4 <almujtaba007@gmail.com>
2023-11-13 07:28:57 -0800

    update 101

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp/ https://raw.github.com/0x41mujt4b4/alx-low_level_programming/master/0x18-dynamic_libraries/wining_numbers.so
+wget -P /tmp/ https://raw.github.com/0x41mujt4b4/alx-low_level_programming/master/0x18-dynamic_libraries/winning_numbers.so
 export LD_PRELOAD=/tmp/winning_numbers.so
